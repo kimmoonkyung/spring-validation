09d427377bc611325dc5a66e3668c7955daf76e1
mxxnkyung@naver.com <mxxnkyung@naver.com>
2021-06-06 15:07:23 +0900

    Initial commit

---
+++ src/main/java/com/example/validation/controller/ApiController.java
@@ -0,0 +1,29 @@
+package com.example.validation.controller;
+
+import com.example.validation.dto.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @PostMapping("/user")
+    public ResponseEntity<User> user(@Valid @RequestBody User user) {
+        System.out.println(user);
+
+        if (user.getAge() >= 90) {
+            System.out.println(user.getAge());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(user);
+        }
+
+        return ResponseEntity.ok(user);
+    }
+
+}
